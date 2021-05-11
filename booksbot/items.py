3f206356e749aed7965ac26b5a5667e24fa72fa4
Prashant Sharma <prashantsharma161198@gmail.com>
2021-05-11 22:02:41 +0530

    feat(booksbot): add BookItem to hold parsed data

--- booksbot/items.py
+++ booksbot/items.py
@@ -1,12 +1,8 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class BooksbotItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class BookItem(scrapy.Item):
+    title = scrapy.Field()
+    price = scrapy.Field()
+    image_url = scrapy.Field()
+    detail_url = scrapy.Field()
--- booksbot/spiders/books_spider.py
+++ booksbot/spiders/books_spider.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from booksbot.items import BookItem
+
 
 class BooksSpider(scrapy.Spider):
     name = "books"
@@ -7,16 +9,14 @@ class BooksSpider(scrapy.Spider):
 
     def parse(self, response):
         for book in response.css("li.col-xs-6"):
-            yield {
-                "title": book.css("h3 a::attr(title)").get(),
-                "price": book.css("p.price_color::text").get(),
-                "image_url": response.urljoin(
-                    book.css("img.thumbnail::attr(src)").get()
-                ),
-                "detail_url": response.urljoin(
-                    book.css("h3 a::attr(href)").get()
-                ),
-            }
+            book_item = BookItem(
+                title=book.css("h3 a::attr(title)").get(),
+                price=book.css("p.price_color::text").get(),
+                image_url=response.urljoin(book.css("img.thumbnail::attr(src)").get()),
+                detail_url=response.urljoin(book.css("h3 a::attr(href)").get()),
+            )
+            yield book_item
+
         next_page = response.css("li.next a::attr(href)").get()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
